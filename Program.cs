35240b9fddb2bf5f5ec152e267414dc71ca035ce
Danny Mussie <dannymussie7@gmail.com>
2021-10-25 17:18:15 -0500

    completed opposites attracts kata

--- Program.cs
+++ Program.cs
@@ -10,3 +10,23 @@ namespace dotnet_hello_world
         }
     }
 }
+
+
+//Opposties attract katas
+using System;
+
+public class LoveDetector
+{
+    public static bool lovefunc(int flower1, int flower2)
+    { 
+        //Moment of truth...
+      if (flower1%2 == 0 || flower2%2 == 0) 
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+}
