32c403a3646883c1b1f74c0a4bc3469598b1804f
Danny Mussie <dannymussie7@gmail.com>
2021-10-25 22:11:39 -0500

    attempted kata #2 and completed kata #3 and passed the tests

--- Program.cs
+++ Program.cs
@@ -30,3 +30,38 @@ public class LoveDetector
       }
     }
 }
+
+// You only need one kata (need clarification on this)
+public class Kata
+{
+  public static bool Check(object[] a, object x)
+  {
+    if (a.Equals(x))
+  {
+    return true;
+  }
+    else
+  {
+    return false;
+  }
+  }
+}
+
+// Sum of postivies kata 
+using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int PositiveSum(int[] arr)
+  {
+    // Your code here
+    int sum = 0;
+    for (int i=0; i < arr.Length; i++)
+    {
+      if (arr[i] > 0) 
+        sum += arr[i];
+    }
+    return sum;
+  }
+}
